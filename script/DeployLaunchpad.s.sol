2da19129c1cc0982fab258a2d0165fa934ad9b92
David Dada <daveproxy80@gmail.com>
2025-08-24 17:41:15 +0100

    chore: update deploy script

--- script/DeployLaunchpad.s.sol
+++ script/DeployLaunchpad.s.sol
@@ -6,13 +6,14 @@ import {Launchpad} from "@shape-nft/Launchpad.sol";
 import {ShapeNFT} from "@shape-nft/ShapeNFT.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
-contract DeployLaunchpad is Script {
+contract DeployLaunchpad is Script, Context {
     function run() public returns (address launchpad_) {
         vm.startBroadcast();
-        UpgradeableBeacon beacon = new UpgradeableBeacon(address(new ShapeNFT()), msg.sender);
+        UpgradeableBeacon beacon = new UpgradeableBeacon(address(new ShapeNFT()), _msgSender());
         BeaconProxy beaconProxy = new BeaconProxy(address(beacon), "");
-        launchpad_ = address(new Launchpad(address(beaconProxy)));
+        launchpad_ = address(new Launchpad(_msgSender(), address(beaconProxy)));
         vm.stopBroadcast();
     }
 }
