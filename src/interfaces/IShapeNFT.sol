a6732df928924de0654f7e3284c398d91ee8b011
David Dada <daveproxy80@gmail.com>
2025-08-24 13:07:08 +0100

    chore: inherit IERC721

--- src/interfaces/IShapeNFT.sol
+++ src/interfaces/IShapeNFT.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-interface IShapeNFT {
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IShapeNFT is IERC721 {
     function initialize(address defaultAdmin, string calldata name, string calldata symbol, string calldata uri)
         external
         returns (address tbaAddress);
