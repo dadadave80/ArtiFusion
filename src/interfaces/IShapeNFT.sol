d2920c8dfd6d8a1dd21a49f3b07e47de9377530b
David Dada <daveproxy80@gmail.com>
2025-08-25 17:40:42 +0100

    uri instead of imageUri

--- src/interfaces/IShapeNFT.sol
+++ src/interfaces/IShapeNFT.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.30;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface IShapeNFT is IERC721 {
-    function initialize(address defaultAdmin, string calldata name, string calldata imageUri)
+    function initialize(address defaultAdmin, string calldata name, string calldata uri)
         external
         returns (address tbaAddress);
-    function mint(address to, string calldata imageUri) external returns (uint256 tokenId, address tbaAddress);
+    function mint(address to, string calldata uri) external returns (uint256 tokenId, address tbaAddress);
 }
